91fc7f248584abb3884e61a592fe5768f700dc13
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-03-07 21:24:10 +0200

    Add AngularFire library and initialize Firebase in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { FirebaseOptions } from 'firebase/app';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment as FirebaseOptions),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyBKNp0I6E7jMFStDfAVd8LRB3G5-WdkCew",
+        authDomain: "helping-hands-85ecf.firebaseapp.com",
+        projectId: "helping-hands-85ecf",
+        storageBucket: "helping-hands-85ecf.appspot.com",
+        messagingSenderId: "446214338847",
+        appId: "1:446214338847:web:9e21fb56d07fd1f8dd582a"
+      }
+};
