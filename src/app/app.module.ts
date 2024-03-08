c0b91525fcc18727eec71c34204bdee4a2f55b60
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-03-08 12:08:09 +0200

    Add angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { FirebaseOptions } from 'firebase/app';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FirebaseOptions } from 'firebase/app';
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment as FirebaseOptions),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
