82dd15b5ffaeb027bde123084e66eb2c90db84c5
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-03-10 16:41:36 +0200

    Create auth-button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthButtonComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
+  ],
+  exports: [
+    AuthButtonComponent
   ]
 })
 export class SharedModule { }
