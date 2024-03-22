abcf48ac6d0f2b59ced3864768bbb17d61060971
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-03-22 21:03:38 +0200

    Implement updateCampaign functionality, add campaign ID upon creation
    Navigate to dashboard after createCampaign()

--- src/app/campaign/campaign.service.ts
+++ src/app/campaign/campaign.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CollectionReference, Firestore, collectionData } from '@angular/fire/firestore';
-import { DocumentData, addDoc, collection } from 'firebase/firestore';
+import { DocumentData, DocumentReference, addDoc, collection, updateDoc } from 'firebase/firestore';
 import { Observable, from } from 'rxjs';
 import { AuthService } from '../user/auth.service';
 import { UserService } from '../user/user.service';
@@ -13,6 +13,7 @@ export class CampaignService{
 
     createCampaign(data: object): Observable<void> {
         const owner = JSON.parse(this.authService.currentUser!).uid;
+        let campaignId = '';
         console.log(owner, { 
             ...data,
             owner
@@ -22,16 +23,25 @@ export class CampaignService{
         const promise = addDoc(this.campaignsCollection, <Campaign>{ 
            ...data,
            owner
-        }).then((data)=>{
-            const campaignId = data.id;
+        })
+        .then((docRef)=>{
+            campaignId = docRef.id;
+            return this.updateCampaign(docRef, {id: campaignId});
+        })
+        .then(()=>{
             return this.userService.updateCampaignsForUser(campaignId, owner)
-        }).then(()=>console.log('Updated user'));
+        })
+        .then(()=>console.log('Updated user'));
 
         return from(promise);
     }
 
     getAllCampaigns():Observable<(DocumentData | (DocumentData & {}))[]>{
         return collectionData(this.campaignsCollection)
+    }
+
+    updateCampaign(docRef: DocumentReference, data: object){
+        return updateDoc(docRef, data);
     }
 }
 
--- src/app/campaign/create-campaign/create-campaign.component.ts
+++ src/app/campaign/create-campaign/create-campaign.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { CampaignService } from '../campaign.service';
-import { AuthService } from 'src/app/user/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-campaign',
@@ -20,13 +20,16 @@ export class CreateCampaignComponent {
     phoneNumber:['', Validators.required],
     region:[null, Validators.required]
   })
-  constructor(private fb: FormBuilder, private campaignService: CampaignService, private authService: AuthService){}
+  constructor(private fb: FormBuilder, private campaignService: CampaignService, private router: Router){}
 
   onSubmit():void{
     console.log(this.createForm.value)
 
     this.campaignService.createCampaign(this.createForm.value).subscribe({
-      next:()=>console.log("Created campaign"),
+      next:()=>{
+        console.log("Created campaign");
+        this.router.navigate(['/campaigns']);
+      },
       error:(error)=> console.log('Error on create campaign', error)
     });
   }
