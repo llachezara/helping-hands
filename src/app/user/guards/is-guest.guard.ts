628f86c8ae292a4599c2658879f32a13024cc00d
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-03-24 11:32:30 +0200

    Refactor isGuestGuard to return an Observable<boolean> instead of a boolean value

--- src/app/user/guards/is-guest.guard.ts
+++ src/app/user/guards/is-guest.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Injectable()
@@ -8,10 +8,9 @@ export class isGuestGuard implements CanActivate{
   constructor(private authService: AuthService, private router: Router){}
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
 
-    if(!this.authService.isLoggedIn) return true;
-
-    this.router.navigate(['/']);
-    console.log('User has already logged in!');
-    return false;
+    return this.authService.user$.pipe(map((user)=> {
+      if(user) this.router.navigate(['/404']);
+      return !user
+    }));
   }
 }
