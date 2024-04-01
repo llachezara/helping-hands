98286979f3d9f0a4607039aea6d6f76352982bc0
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-04-02 00:50:48 +0300

    Add error-handling in register component, implement cleanup in ngOnDestroy
    Add error-handling in register component using div.error for backend errors and password mismatch. Utilize existing IsEmailDirective and isPasswordDirective validator directives for input validation. Create an array to store subscriptions and unsubscribe from subscriptions in ngOnDestroy.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,24 +1,24 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
 import { UserCredential } from 'firebase/auth';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnDestroy{
   @ViewChild('registerForm') form: NgForm | undefined;
-
   errorMessage: string | null = null;
- 
-  constructor(private authService: AuthService, private router: Router, private userService: UserService){}
+  subscriptions: Subscription[] = [];
+  
+  constructor(private authService: AuthService, private router: Router){}
 
   onSubmit() {
-     //TODO: Validate form
      if (!this.form) {
        return
      }
@@ -26,7 +26,7 @@ export class RegisterComponent {
      const {email, passGroup} = this.form.value;
      const {password, rePassword} = passGroup;
 
-    this.authService.register(email, password).subscribe({
+    const registerSubscription = this.authService.register(email, password).subscribe({
         next: ()=> {
           console.log('Registered');
           this.errorMessage = null;
@@ -35,11 +35,19 @@ export class RegisterComponent {
         error: (error)=> {
           console.log(error);
           this.errorMessage = error.message;
+          this.form!.resetForm({email: email, password: ''})
+          setTimeout(()=>{
+            this.errorMessage = ""
+          }, 5000);
         }
     });
 
+    this.subscriptions.push(registerSubscription);
     
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe())
+  }
 }
 
